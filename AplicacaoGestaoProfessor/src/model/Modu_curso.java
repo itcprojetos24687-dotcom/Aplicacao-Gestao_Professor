132d9a9c5157ffc524687c30d4193093aae34afd
mapotere <mapotere@mapotere-candy>
2026-06-14 21:21:52 +0200

    Adicionar ficheiros da branch

---
+++ AplicacaoGestaoProfessor/src/model/Modu_curso.java
@@ -0,0 +1,5 @@
+package model;
+
+public class Modu_curso {
+
+}
---
+++ AplicacaoGestaoProfessor/src/model/Turma.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Turma implements Comparable<Turma> {
+	private int codigo_turma;
+	private String nome_turma;
+	private int ano_ingresso;
+	private int codigo_curso;
+	
+	public Turma() {
+		
+	}
+	
+	public Turma(int codigo_turma,String nome_turma, int ano_ingresso, int codigo_curso ) {
+		this.codigo_turma = codigo_turma;
+		this.nome_turma = nome_turma;
+		this.ano_ingresso = ano_ingresso;
+		this.codigo_curso = codigo_curso;
+	}
+	public int getCodigo_turma() {
+		return codigo_turma;
+	}
+	public void sentcodigo_turma(int codigo_turma) {
+		this.codigo_turma = codigo_turma;
+	}
+	public String getNome_turma() {
+		return nome_turma;
+	}
+	public void setNome_turma(String nome_turma) {
+		this.nome_turma = nome_turma;
+	}
+	public int getAno_ingresso() {
+		return ano_ingresso;
+	}
+	public void set(int ano_ingresso) {
+		this.ano_ingresso = ano_ingresso;
+	}
+	public int getcodigo_curso() {
+		return codigo_curso;
+	}
+	public void setcodigo_curso(int codigo_curso) {
+		this.codigo_curso = codigo_curso;
+	}
+	public int compareTo(Turma turma) {
+		if (this.codigo_turma > turma.codigo_turma)
+			return 1;
+		if (this.codigo_turma < turma.codigo_turma)
+			return -1;
+		return 0;
+
+}
+}
