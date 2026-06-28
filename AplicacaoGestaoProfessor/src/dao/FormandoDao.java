d2cf278601988ac2c823b0a657af5edf79503b7c
mapotere <mapotere@mapotere-candy>
2026-06-28 11:40:45 +0200

    nova atualizacao

---
+++ AplicacaoGestaoProfessor/src/dao/FormandoDao.java
@@ -0,0 +1,143 @@
+package dao;
+import model.Formando;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+
+public class FormandoDao {
+
+	Connection con = null;
+	public void cadastrarFormando(Formando formando) throws ExceptionDao{
+		
+		
+		String sql = "insert into Formando(nome, apelido, contacto, email, bi)" + "values(?,?,?,?,?)";
+		PreparedStatement InsertFormando = null;
+		try {
+			
+			con = new Conexao().getConnection();
+			InsertFormando = con.prepareStatement(sql);
+			InsertFormando.setString(1, formando.getNome());
+			InsertFormando.setString(2, formando.getApelido());
+			InsertFormando.setInt(3, formando.getContacto());
+			InsertFormando.setString(4, formando.getEmail());
+			InsertFormando.setString(5, formando.getBi());
+			InsertFormando.execute();
+			
+		}catch(SQLException e) {
+			JOptionPane.showMessageDialog(null, "Erro ao inserir dado");
+			throw new ExceptionDao("Erro ao inserir dados :" + e );
+		}finally {
+			try {
+				if(InsertFormando != null) {
+					InsertFormando.close();
+					JOptionPane.showMessageDialog(null, "Statemente fechado com sucesso");
+				}
+			}catch(SQLException sq) {
+				throw new ExceptionDao("Erro ao fechar o statement");
+			}
+			try {
+			if (con != null) {
+				con.close();
+				JOptionPane.showMessageDialog(null, "Conexao fechado com Sucesso" );
+			}
+		}catch(SQLException f) {
+			throw new ExceptionDao("Erro ao fechar a conexao ");
+		}
+	}
+}
+	public static void main(String[] args) throws ExceptionDao {
+		
+	}
+	public ArrayList<Formando> listarFormando(String nome )throws ExceptionDao{
+		String sql = "select * from formando where nome like '%" + nome + "%'";
+		PreparedStatement listarFormando = null;
+		ArrayList <Formando> formandos = null;
+		
+		try {
+		con = new Conexao().getConnection();
+			listarFormando = con.prepareStatement(sql);
+				ResultSet rs = listarFormando.executeQuery();
+				
+				if (rs != null) {
+					formandos = new ArrayList();
+					while(rs.next()) {
+					Formando formando = new Formando(); 
+					
+					formando.setCodigo(rs.getInt("codigo"));
+					formando.setNome(rs.getString("nome"));
+					formando.setApelido(rs.getString("apelido"));
+					formando.setContacto(rs.getInt("contacto"));
+					formando.setEmail(rs.getString("Email"));
+					formando.setBi(rs.getString("Bi"));
+					formandos.add(formando);
+					JOptionPane.showMessageDialog(null, "Adicionado com sucesso");
+					
+					}
+				}
+		}catch(SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Erro ao selecionar");
+			throw new ExceptionDao("Erro ao selecionar dados " + ex);
+		}finally {
+			try {
+				if(listarFormando != null) {
+					listarFormando.close();
+					JOptionPane.showMessageDialog(null, "statement fechado com sucesso");
+				}
+			}catch(Exception es) {
+				JOptionPane.showMessageDialog(null, "Erro ao fechar statement");
+				throw new ExceptionDao("Erro ao fechar o statement: " + es);
+			}
+			try {
+				if(con != null) {
+					con.close();
+					JOptionPane.showMessageDialog(null, "Conexao fechado com sucesso");
+				}
+			}
+			catch(SQLException sq) {
+				JOptionPane.showMessageDialog(null, "Erro ao fechar conexao");
+				throw new ExceptionDao("Erro ao fechar conexao: " + sql);
+			}
+		}
+		return formandos;
+			
+	}
+	public void atualizarFormando(Formando formando) {
+		String  sql = "update Formando set nome = ?, apelido = ?, contacto = ?, email = ?, Bi = ?";
+		PreparedStatement alterarFormando = null;
+		
+		try {
+			con = new Conexao().getConnection();
+			alterarFormando = con.prepareStatement(sql);
+			alterarFormando.setString(1, formando.getNome());
+			alterarFormando.setString(2, formando.getApelido());
+			alterarFormando.setInt(3, formando.getContacto());
+			alterarFormando.setString(3, formando.getEmail());
+			alterarFormando.setString(4, formando.getBi());
+		}catch(SQLException e) {
+			JOptionPane.showMessageDialog(null,"Erro ao alterar");
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if(alterarFormando != null) {
+					alterarFormando.close();
+					JOptionPane.showMessageDialog(null, "Fechado com sucesso");
+				}
+			}catch(SQLException sq) {
+				sq.printStackTrace();
+			}
+			try {
+				if(con != null) {
+					con.close();
+					JOptionPane.showMessageDialog(null, "Fechado com sucesso");
+				}
+			}catch(SQLException l) {
+				JOptionPane.showMessageDialog(null, "Falha de fechado ");
+			}
+		}
+	}
+}
---
+++ AplicacaoGestaoProfessor/src/dao/ModuloDao.java
@@ -0,0 +1,136 @@
+package dao;
+import model.Modulo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+
+
+public class ModuloDao {
+	
+	Connection con = null;
+	public void cadastrarModulo(Modulo modulo) throws ExceptionDao{
+		
+		String sql = "insert into Modulo(nome, carga_horaria)" + "values(?,?)";
+		PreparedStatement InsertModulo = null;
+		try {
+			
+			con = new Conexao().getConnection();
+			InsertModulo = con.prepareStatement(sql);
+			InsertModulo.setString(1, modulo.getNome());
+			InsertModulo.setInt(2, modulo.getCarga_horaria());
+			InsertModulo.execute();
+			
+		}catch(SQLException e) {
+			JOptionPane.showMessageDialog(null,"Statemente fechado com sucesso");
+			throw new ExceptionDao ("Erro ao inserir dados :" + e);
+		}finally {
+			try {
+				if(InsertModulo != null) {
+					InsertModulo.close();
+					JOptionPane.showMessageDialog(null, "Statemente fechado com sucesso");
+				}
+			}catch(SQLException sq) {
+				throw new ExceptionDao("Erro ao fechar o statement");
+			}
+			try {
+				if (con != null) {
+					con.close();
+					JOptionPane.showMessageDialog(null, "Conexao fechado com sucesso");
+				}
+			}catch(SQLException f) {
+				throw new ExceptionDao("Erro ao fechar a conexao");
+			}
+		}
+	}
+	public static void main(String[] args) throws ExceptionDao {
+		
+	}
+	public ArrayList<Modulo>listarModulo(String nome) throws ExceptionDao{
+		String sql = "select * from Modulo whre nome like '%" + nome +"%'";
+		PreparedStatement listarModulo = null;
+		ArrayList <Modulo> modulos = null;
+		
+		
+		try {
+			con = new Conexao().getConnection();
+			listarModulo = con.prepareStatement(sql);
+			ResultSet rs = listarModulo.executeQuery();
+			
+			if (rs != null) {
+				modulos = new ArrayList();
+				while(rs.next()) {
+					Modulo modulo = new Modulo();
+					
+					modulo.setCodigo(rs.getInt("codigo"));
+					modulo.setNome(rs.getString("nome"));
+					modulo.setCarga_horaria(rs.getInt("Carga_horaria"));
+					modulos.add(modulo);
+					JOptionPane.showMessageDialog(null, "Adicionar com sucesso");
+					
+				}
+			}
+		}catch(SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Erro ao selecionar");
+			throw new ExceptionDao("Erro ao selecionar" + ex );
+			
+		}finally {
+			try {
+				if(listarModulo != null) {
+					listarModulo.close();
+					JOptionPane.showMessageDialog(null, "statement fechado com sucesso");
+				}
+			}catch(Exception es) {
+				JOptionPane.showMessageDialog(null, "Erro ao fechar statement ");
+				throw new ExceptionDao("Erro ao fechar o statement" + es);
+			}
+			try {
+				if(con != null) {
+					con.close();
+					JOptionPane.showMessageDialog(null, "Conexao fechado com sucesso");
+				}
+			}
+			catch(SQLException sq) {
+				JOptionPane.showMessageDialog(null, "Erro ao fechar conexao");
+				throw new ExceptionDao("Erro ao fechar conexao: " + sq);
+			}
+		}
+		return modulos;
+	}
+	public void atualizarModulo(Modulo modulo) {
+		String sql = "update Modulo set nome = ?, carga_horaria= ? where codigo = ?";
+		PreparedStatement alterarModulo = null;
+		
+		try {
+			con = new Conexao().getConnection();
+			alterarModulo = con.prepareStatement(sql);
+			alterarModulo.setString(1, modulo.getNome());
+			alterarModulo.setInt(2, modulo.getCarga_horaria());
+			JOptionPane.showMessageDialog(null, "Alterado com sucesso");
+		}catch(SQLException e) {
+			JOptionPane.showMessageDialog(null, "Erro ao alterar");
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if(alterarModulo != null) {
+					alterarModulo.close();
+					JOptionPane.showMessageDialog(null, "Fechado com sucesso");
+				}
+			}catch(SQLException sq) {
+				sq.printStackTrace();
+			}
+			try {
+				if(con != null) {
+					con.close();
+					JOptionPane.showMessageDialog(null, "Fechado com sucesso");
+				}
+			}catch(SQLException l) {
+				JOptionPane.showMessageDialog(null, "Falha de fechado ");
+			}
+		}
+	}
+}
