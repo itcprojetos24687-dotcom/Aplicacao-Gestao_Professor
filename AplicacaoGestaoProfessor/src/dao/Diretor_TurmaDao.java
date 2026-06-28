7af32e78c0a3a76a74815b2d47ca8f74c2ddd2fb
malik-dev <malik-dev@fedora>
2026-06-28 19:57:13 +0200

    Feature<crud>: fazendo os metodso do crud no Diretor de Turma

---
+++ AplicacaoGestaoProfessor/src/dao/Diretor_TurmaDao.java
@@ -0,0 +1,188 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import model.Diretor_Turma;
+import model.Formador;
+
+
+public class Diretor_TurmaDao {
+	Connection con = null;
+	public void cadastrarDiretor_Turma(Formador formador) throws ExceptionDao {
+		String sql = "insert into Formador(nome, apelido, email, genero, estadoCivil, contacto, salario)" + "values(?,?,?,?,?,?,?)";
+		PreparedStatement InsertFormador = null;
+		try {
+			
+			con = new Conexao().getConnection();
+			InsertFormador = con.prepareStatement(sql);
+			Diretor_Turma diretor = new Diretor_Turma(formador);
+			InsertFormador.setString(1, formador.getNome());
+			InsertFormador.setString(2, formador.getApelido());
+			InsertFormador.setString(3, formador.getEmail() );
+			InsertFormador.setString(4, formador.getGenero());
+			InsertFormador.setString(5, formador.getEstadoCivil());
+			InsertFormador.setInt(6, formador.getContacto());
+			InsertFormador.setDouble(7, formador.getSalario());
+			InsertFormador.execute();
+			
+		}catch(SQLException e) {
+			//JOptionPane.showMessageDialog(null, "Erro ao inserir dado");
+			 throw new ExceptionDao("Erro ao inserir dados :" + e);
+		}finally {
+			try {
+				if(InsertFormador != null) {
+					InsertFormador.close();
+					//JOptionPane.showMessageDialog(null, "Statemente fechado com sucesso");
+				}
+			}catch(SQLException sq) {
+				throw new ExceptionDao("Erro ao fechar o statement");
+			}
+			try {
+				if (con != null) {
+					con.close();
+					//JOptionPane.showMessageDialog(null, "Conexao fechado com sucesso");
+				}
+			}catch(SQLException f) {
+				throw new ExceptionDao("Erro ao fechar a conexao ");
+			}
+		}
+	}
+	public ArrayList<Diretor_Turma> listaDiretor_Turma(String nome) throws ExceptionDao{
+
+		String sql = "select * from Formador where nome like '%" + nome + "%'";
+		PreparedStatement listarFormador = null;
+		ArrayList <Formador> formadores = null;
+		
+		
+		try {
+			con = new Conexao().getConnection();
+			listarFormador = con.prepareStatement(sql);
+			ResultSet rs = listarFormador.executeQuery();
+			
+			if (rs != null) {
+				formadores = new ArrayList();
+				while(rs.next()) {
+					Formador formador = new Formador();
+					
+					formador.setCodigo(rs.getInt("codigo"));
+					formador.setNome(rs.getString("nome"));
+					formador.setApelido(rs.getString("apelido"));
+					formador.setEmail(rs.getString("email"));
+					formador.setGenero(rs.getString("genero"));
+					formador.setEstadoCivil(rs.getString("estadoCivil"));
+					formador.setContacto(rs.getInt("contacto"));
+					formador.setSalario(rs.getDouble("salario"));
+					formadores.add(formador);
+					//JOptionPane.showMessageDialog(null, "Adicionado com sucesso");
+				}
+			}
+			
+			
+		}catch(SQLException ex) {
+			//JOptionPane.showMessageDialog(null, "Erro ao selecionar");
+			throw new ExceptionDao("Erro ao selecionar dados" + ex);
+			
+		}finally {
+			try {
+				if(listarFormador != null) {
+					listarFormador.close();
+					//JOptionPane.showMessageDialog(null, "statement fechado com sucesso");
+				}
+			}catch(Exception es) {
+				//JOptionPane.showMessageDialog(null, "Erro ao fechar statement");
+				throw new ExceptionDao("Erro ao fechar o statement: "+ es);
+			}
+			try {
+				if(con != null) {
+					con.close();
+					//JOptionPane.showMessageDialog(null, "Conexao fechado com sucesso");
+				}			
+			}
+			catch(SQLException sq) {
+				//JOptionPane.showMessageDialog(null, "Erro ao fechar conexao");
+				throw new ExceptionDao("Erro ao fechar conexao: " + sq);
+			}
+		}
+		return formadores;
+		
+	}
+	public void atualizarDiretor_Turma(Diretor_Turma diretor) {
+		String sql = "update Formador set nome = ?, apelido = ?, email = ?, genero = ?, estadoCivil = ?, contacto = ?, salario = ? where codigo = ?";
+		PreparedStatement alterarFormador = null;
+		
+		try {
+			con = new Conexao().getConnection();
+			alterarFormador = con.prepareStatement(sql);
+			alterarFormador.setString(1, formador.getNome());
+			alterarFormador.setString(2, formador.getApelido());
+			alterarFormador.setString(3, formador.getEmail());
+			alterarFormador.setString(4, formador.getGenero());
+			alterarFormador.setString(5, formador.getEstadoCivil());
+			alterarFormador.setInt(6, formador.getContacto());
+			alterarFormador.setDouble(7, formador.getSalario());
+			alterarFormador.setInt(8, formador.getCodigo());
+			alterarFormador.executeUpdate();
+			//JOptionPane.showMessageDialog(null,"Alterado com sucesso");
+		}catch(SQLException e) {
+			//JOptionPane.showMessageDialog(null,"Erro ao alterar");
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if(alterarFormador != null) {
+					alterarFormador.close();
+					//JOptionPane.showMessageDialog(null, "Fechado com sucesso");
+				}
+			}catch(SQLException sq) {
+				sq.printStackTrace();
+			}
+			try {
+				if(con != null) {
+					con.close();
+					//JOptionPane.showMessageDialog(null, "Fechado com sucesso");
+				}
+			}catch(SQLException l) {
+				//JOptionPane.showMessageDialog(null, "Falha de fechado ");
+			}
+			
+		}
+	}
+	public void apagarDiretor_Turma(Diretor_Turma diretor) throws ExceptionDao {
+
+		String sql = "delete from Formador where codigo=?";
+		PreparedStatement apagarFormador = null;
+		try {
+			
+			con = new Conexao().getConnection();
+			apagarFormador = con.prepareStatement(sql);
+			apagarFormador.setInt(1, formador.getCodigo());
+			apagarFormador.executeUpdate();
+			//JOptionPane.showMessageDialog(null, "Apagado com sucesso");
+			
+		}catch(SQLException e) {
+			//JOptionPane.showMessageDialog(null, "Erro ao inserir dado");
+			 throw new ExceptionDao("Erro ao inserir dados :" + e);
+		}finally {
+			try {
+				if(apagarFormador != null) {
+					apagarFormador.close();
+					//JOptionPane.showMessageDialog(null, "Statemente fechado com sucesso");
+				}
+			}catch(SQLException sq) {
+				throw new ExceptionDao("Erro ao fechar o statement");
+			}
+			try {
+				if (con != null) {
+					con.close();
+					//JOptionPane.showMessageDialog(null, "Conexao fechado com sucesso");
+				}
+			}catch(SQLException f) {
+				throw new ExceptionDao("Erro ao fechar a conexao ");
+			}
+		}
+	}
+}
--- AplicacaoGestaoProfessor/src/model/Diretor_Turma.java
+++ AplicacaoGestaoProfessor/src/model/Diretor_Turma.java
@@ -1,8 +1,32 @@
 package model;
+import java.util.ArrayList;
 
-public class Diretor_Turma extends Formador{
-	
-	public Diretor_Turma(String nome, String apelido, String email, String genero, String estadoCivil, int contacto, double salario) {
-		super(nome, apelido, email, genero, estadoCivil, contacto, salario);
+import dao.*;
+
+public class Diretor_Turma {
+	private Formador formador;
+	public Diretor_Turma(Formador formador) {
+		this.formador= formador;
 	}
+	
+	public Formador getFomador() {
+		return formador;
+	}
+	public void setFormador(Formador formador) {
+		this.formador=formador;
+	}
+	
+	public void cadastrarDiretor_Turma(Diretor_Turma diretor)throws ExceptionDao{
+		new Diretor_TurmaDao().cadastrarDiretor_Turma(diretor);;
+	}
+	public ArrayList<Diretor_Turmas> listarDiretor_Turma(Diretor_Turma diretor) throws ExceptionDao{
+		return new Diretor_TurmaDao().listarDiretor_Turma(diretor);
+	}
+	public void atualizarDiretor_Turma(Diretor_Turma diretor) throws ExceptionDao{
+		new Diretor_TurmaDao().atualizarDiretor_Turma(diretor);
+	}
+	public void apagarCampo(Diretor_Turma diretor)throws ExceptionDao{
+		new Diretor_TurmaDao().apagarDiretor_Turma(diretor);
+	}
+	
 }
